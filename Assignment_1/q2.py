ebd39cf2402664ee1a3c74d26840b4a94d11f7db
ramonrsv <ramonesibello@gmail.com>
2017-10-03 23:54:53 -0400

    Minor style change to Q2

--- Assignment_1/q2.py
+++ Assignment_1/q2.py
@@ -96,6 +96,8 @@ def run_k_fold(x, y, taus, k):
 
 
 if __name__ == "__main__":
+    np.random.seed(0)  # Make runs repeatable
+
     # In this exercise we fixed lambda (hard coded to 1e-5) and only set tau value.
     # Feel free to play with lambda as well if you wish
     taus = np.logspace(1.0, 3, 200)
