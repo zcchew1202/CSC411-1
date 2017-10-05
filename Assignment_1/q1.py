bdf5804e88b8f15a2dce559ec79f6e4c9d9fc4b9
ramonrsv <ramonesibello@gmail.com>
2017-10-05 04:23:02 -0400

    Changing the third error metric calculation in Q1 to Root Mean Square

--- Assignment_1/q1.py
+++ Assignment_1/q1.py
@@ -1,5 +1,5 @@
 from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_squared_log_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
 import numpy as np
 import colorsys
@@ -102,12 +102,12 @@ def main():
     skl_regr.fit(X_train, y_train)
     skl_y = skl_regr.predict(X_test)
 
-    print("Mean Squared Error, Mean Absolute Error, Mean Squared Log Error")
+    print("Mean Squared Error, Mean Absolute Error, Root Mean Squared Error")
     for y_hat, name in [(y, "RSV Linear Regression"), (skl_y, "sklearn.linear_model.LinearRegression")]:
         print(name + str(": ") + ', '.join([
             str(mean_squared_error(y_test, y_hat)),
             str(mean_absolute_error(y_test, y_hat)),
-            str(mean_squared_log_error(y_test, y_hat))
+            str(np.sqrt(mean_squared_error(y_test, y_hat)))
         ]))
 
     for i, wi in enumerate(w[1:]):
